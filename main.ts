859fff6b7b107ecec769dcc728f3099a6562ca95
erdelbusxxx <127854323+erdelbusxxx@users.noreply.github.com>
2023-03-14 13:25:34 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+let velocidad = 40
 basic.forever(function () {
-	
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
+        maqueen.motorStop(maqueen.Motors.M1)
+    }
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velocidad)
+    }
+})
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorStop(maqueen.Motors.M2)
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, velocidad)
+        }
+    }
 })
